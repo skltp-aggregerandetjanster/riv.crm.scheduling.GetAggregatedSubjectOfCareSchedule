f5d0232013b0b96cb41051df412d9ed134f56799
magnus.larsson@callistaenterprise.se <magnus.larsson@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2013-03-03 07:02:07 +0000

    agp: added AbstractTestConsumer.java in agp-test-common, started to us it in aggregating service for remisstatus and tidbokning

--- src/test/java/se/skltp/agp/tidbokning/TidbokningTestConsumer.java
+++ src/test/java/se/skltp/agp/tidbokning/TidbokningTestConsumer.java
@@ -3,12 +3,8 @@ package se.skltp.agp.tidbokning;
 import static se.skltp.agp.TidbokningMuleServer.getAddress;
 import static se.skltp.agp.test.producer.TestProducerDb.TEST_RR_ID_ONE_HIT;
 
-import java.net.URL;
-
 import javax.xml.ws.Holder;
 
-import org.apache.cxf.bus.spring.SpringBusFactory;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,15 +13,14 @@ import se.riv.crm.scheduling.getsubjectofcarescheduleresponder.v1.GetSubjectOfCa
 import se.riv.crm.scheduling.getsubjectofcarescheduleresponder.v1.GetSubjectOfCareScheduleType;
 import se.riv.interoperability.headers.v1.ActorType;
 import se.riv.interoperability.headers.v1.ActorTypeEnum;
+import se.skltp.agp.test.consumer.AbstractTestConsumer;
 import se.skltp.agp.test.consumer.SoapHeaderCxfInterceptor;
 import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusType;
 
-public class TidbokningTestConsumer {
+public class TidbokningTestConsumer  extends AbstractTestConsumer<GetSubjectOfCareScheduleResponderInterface> {
 
 	private static final Logger log = LoggerFactory.getLogger(TidbokningTestConsumer.class);
 
-	private GetSubjectOfCareScheduleResponderInterface _service = null;
-	
 	public static void main(String[] args) {
 		String serviceAddress = getAddress("SERVICE_INBOUND_URL");
 		String personnummer = TEST_RR_ID_ONE_HIT;
@@ -33,56 +28,28 @@ public class TidbokningTestConsumer {
 		TidbokningTestConsumer consumer = new TidbokningTestConsumer(serviceAddress);
 		Holder<GetSubjectOfCareScheduleResponseType> responseHolder = new Holder<GetSubjectOfCareScheduleResponseType>();
 		Holder<ProcessingStatusType> processingStatusHolder = new Holder<ProcessingStatusType>();
-		long now = System.currentTimeMillis();
-		consumer.callService("logical-adress", personnummer, processingStatusHolder, responseHolder);
-		log.info("Returned #timeslots = " + responseHolder.value.getTimeslotDetail().size() + " in " + (System.currentTimeMillis() - now) + " ms.");
 
-		now = System.currentTimeMillis();
 		consumer.callService("logical-adress", personnummer, processingStatusHolder, responseHolder);
-		log.info("Returned #timeslots = " + responseHolder.value.getTimeslotDetail().size() + " in " + (System.currentTimeMillis() - now) + " ms.");
-	
-		now = System.currentTimeMillis();
-		consumer.callService("logical-adress", personnummer, processingStatusHolder, responseHolder);
-		log.info("Returned #timeslots = " + responseHolder.value.getTimeslotDetail().size() + " in " + (System.currentTimeMillis() - now) + " ms.");
-	
-		now = System.currentTimeMillis();
-		consumer.callService("logical-adress", personnummer, processingStatusHolder, responseHolder);
-		log.info("Returned #timeslots = " + responseHolder.value.getTimeslotDetail().size() + " in " + (System.currentTimeMillis() - now) + " ms.");
-	
+		log.info("Returned #timeslots = " + responseHolder.value.getTimeslotDetail().size());
+
 	}
 	
 	public TidbokningTestConsumer(String serviceAddress) {
-		JaxWsProxyFactoryBean proxyFactory = new JaxWsProxyFactoryBean();
-		proxyFactory.setServiceClass(GetSubjectOfCareScheduleResponderInterface.class);
-		proxyFactory.setAddress(serviceAddress);
-		
-		// Used for HTTPS
-		SpringBusFactory bf = new SpringBusFactory();
-		URL cxfConfig = TidbokningTestConsumer.class.getClassLoader().getResource("cxf-test-consumer-config.xml");
-		if (cxfConfig != null) {
-			proxyFactory.setBus(bf.createBus(cxfConfig));
-		}
-
-		_service  = (GetSubjectOfCareScheduleResponderInterface) proxyFactory.create(); 
-
-//		try {
-//            URL url =  new URL(serviceAddress + "?wsdl");
-//            _service = new GetAggregatedSubjectOfCareScheduleResponderService(url).getGetAggregatedSubjectOfCareScheduleResponderPort();
-//        } catch (MalformedURLException e) {
-//            throw new RuntimeException("Malformed URL Exception: " + e.getMessage());
-//        }
+	    
+		// Setup a web service proxy for communication using HTTPS with Mutual Authentication
+		super(GetSubjectOfCareScheduleResponderInterface.class, serviceAddress);
 	}
 
-	public void callService(String logicalAddress, String id, Holder<ProcessingStatusType> processingStatusHolder, Holder<GetSubjectOfCareScheduleResponseType> responseHolder) {
+	public void callService(String logicalAddress, String registeredResidentId, Holder<ProcessingStatusType> processingStatusHolder, Holder<GetSubjectOfCareScheduleResponseType> responseHolder) {
 
-		log.debug("Calling GetSubjectOfCareSchedule-soap-service with id = {}", id);
+		log.debug("Calling GetSubjectOfCareSchedule-soap-service with Registered Resident Id = {}", registeredResidentId);
 		
 		ActorType actor = new ActorType();
-		actor.setActorId(id);
+		actor.setActorId(registeredResidentId);
 		actor.setActorType(ActorTypeEnum.SUBJECT_OF_CARE);
 		
 		GetSubjectOfCareScheduleType request = new GetSubjectOfCareScheduleType();
-		request.setSubjectOfCare(id);
+		request.setSubjectOfCare(registeredResidentId);
 
 		GetSubjectOfCareScheduleResponseType response = _service.getSubjectOfCareSchedule(logicalAddress, actor, request);
 		responseHolder.value = response;
