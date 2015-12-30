bcc73935da57ff4c83b04a4a30aef19e5791c074
Martin Flower <martin.flower@callistaenterprise.se>
2015-12-30 10:31:26 +0100

    SERVICE-415 - Assert xml contents similar with latest xmlunit

--- GetAggregatedSubjectOfCareSchedule/src/test/java/se/skltp/agp/cache/CacheEntryUtilTest.java
+++ GetAggregatedSubjectOfCareSchedule/src/test/java/se/skltp/agp/cache/CacheEntryUtilTest.java
@@ -17,11 +17,18 @@ import java.util.List;
 
 import org.junit.Test;
 import org.mule.api.MuleEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.soitoolkit.commons.mule.jaxb.JaxbUtil;
+import org.xmlunit.builder.DiffBuilder;
+import org.xmlunit.diff.DefaultNodeMatcher;
+import org.xmlunit.diff.Diff;
+import org.xmlunit.diff.ElementSelectors;
 
 import se.riv.crm.scheduling.getsubjectofcarescheduleresponder.v1.GetSubjectOfCareScheduleResponseType;
 import se.riv.crm.scheduling.getsubjectofcarescheduleresponder.v1.ObjectFactory;
 import se.riv.crm.scheduling.v1.TimeslotType;
+import se.skltp.agp.TidbokningMuleServer;
 import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusRecordType;
 import se.skltp.agp.riv.interoperability.headers.v1.ProcessingStatusType;
 import se.skltp.agp.test.producer.TestProducerDb;
@@ -29,7 +36,9 @@ import se.skltp.agp.tidbokning.TidbokningTestProducerDb;
 
 public class CacheEntryUtilTest {
 
-	private MyTestUtil testUtil = new MyTestUtil();
+    private static final Logger logger = LoggerFactory.getLogger(CacheEntryUtilTest.class);
+
+    private MyTestUtil testUtil = new MyTestUtil();
 	
 	private JaxbUtil ju = new JaxbUtil(GetSubjectOfCareScheduleResponseType.class);
 	private ObjectFactory of = new ObjectFactory();
@@ -94,14 +103,25 @@ public class CacheEntryUtilTest {
 		CacheEntryUtil ce = new CacheEntryUtil(e);
 
 		// Assert the expected body-part
-		assertEquals(normalizeXmlString(MyTestUtil.singleXmlBody), normalizeXmlString(ce.getSoapBody()));
+		String expectedXmlString = MyTestUtil.singleXmlBody;
+		String actualXmlString   = ce.getSoapBody();
+		Diff diff = DiffBuilder.compare(expectedXmlString).withTest(actualXmlString)
+		        .checkForSimilar() // a different order is always 'similar' not equals.
+		        .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byNameAndText))
+		        .build();
+		assertFalse("expected and actual should be similar " + diff.toString(), diff.hasDifferences());		
 		
 		// Update the body
 		String newBodyXml = "<ns1:GetSubjectOfCareScheduleResponse xmlns:ns1=\"urn:riv:crm:scheduling:GetSubjectOfCareScheduleResponder:1\"/>";
 		ce.setSoapBody(newBodyXml);
-
-		// Assert that the update is reflected in the cacheEntry
-		assertEquals(normalizeXmlString(newBodyXml), normalizeXmlString(ce.getSoapBody()));
+		// and assert that the update is reflected in the cacheEntry
+        expectedXmlString = newBodyXml;
+        actualXmlString   = ce.getSoapBody();
+        diff = DiffBuilder.compare(expectedXmlString).withTest(actualXmlString)
+                .checkForSimilar() // a different order is always 'similar' not equals.
+                .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byNameAndText))
+                .build();
+        assertFalse("expected and actual should be similar " + diff.toString(), diff.hasDifferences());     
 		
 		// Also assert that the update is reflected in the origin MuleEvent
 		String updatedPayload = (String)e.getMessage().getPayload();
